c0489246cff1b177673502327239b46c9b17aa20
sky <yuanxiaofei_apache@163.com>
2014-09-02 16:16:15 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebsocketRailsDemo::Application.config.secret_key_base = '7fb308411e383d87b0c0311ef2ee6cdccaaf7b646666c5ea33b2301cbffad639a54678488b62e3b57e65cd0ada33319ca60d47e3139b2796dd262b9e7bdfc1c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebsocketRailsDemo::Application.config.session_store :cookie_store, key: '_websocket_rails_demo_session'
